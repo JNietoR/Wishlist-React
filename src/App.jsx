4095c1446df0f872c41357f3786519c6babd49ae
JNietoR <jorge.nieto35@gmail.com>
2023-11-12 13:12:07 +0100

    renderizado de la lista de deseos

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  
+const wishes = [
+    {text : 'Travel to the moon', done:false},
+    {text : 'Pay the gym', done:true},
+    {text : 'Go to the gym', done:false},
+  ];
 
-  return (
-    <>
-      <h1>Mi Wishlist</h1>
-    </>
-  )
-}
+const App = () => (
+    <div>
+        <h1>My Wishlist</h1>
+        <fieldset>
+            <legend>New wish</legend>
+            <input placeholder="Enter your wish here"/>
+        </fieldset>
+        <ul>
+          {wishes.map(wish => (
+            <li>{wish.text}</li>
+          ))}
+        </ul>
+    </div>
+)
 
 export default App
